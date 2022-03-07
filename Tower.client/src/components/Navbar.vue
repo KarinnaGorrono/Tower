62400ad1aeffc8900de8fd03f0fd3eb7b685f3e3
KarinnaGorrono <karinna.gorrono@gmail.com>
2022-03-07 14:40:22 -0700

    complete build

--- Tower.client/src/components/Navbar.vue
+++ Tower.client/src/components/Navbar.vue
@@ -2,7 +2,7 @@
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark px-3">
     <router-link class="navbar-brand d-flex" :to="{ name: 'Home' }">
       <div class="d-flex flex-column align-items-center">
-        <img alt="logo" src="../assets/img/cw-logo.png" height="45" />
+        <!-- <img alt="logo" src="../assets/img/cw-logo.png" height="45" /> -->
       </div>
     </router-link>
     <button
@@ -68,11 +68,7 @@
               </div>
             </router-link>
             <div
-              class="
-                list-group-item list-group-item-action
-                hover
-                text-danger
-              "
+              class="list-group-item list-group-item-action hover text-danger"
               @click="logout"
             >
               <i class="mdi mdi-logout"></i>
@@ -86,22 +82,22 @@
 </template>
 
 <script>
-import { AuthService } from '../services/AuthService'
-import { AppState } from '../AppState'
-import { computed } from 'vue'
+import { AuthService } from "../services/AuthService";
+import { AppState } from "../AppState";
+import { computed } from "vue";
 export default {
   setup() {
     return {
       user: computed(() => AppState.user),
       async login() {
-        AuthService.loginWithPopup()
+        AuthService.loginWithPopup();
       },
       async logout() {
-        AuthService.logout({ returnTo: window.location.origin })
-      }
-    }
-  }
-}
+        AuthService.logout({ returnTo: window.location.origin });
+      },
+    };
+  },
+};
 </script>
 
 <style scoped>
